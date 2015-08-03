478eaeb9757ae9443c0fad1cbf2c0e2769999346
Niladri-B <nilan666@gmail.com>
2015-08-03 10:48:10 +0200

    adding filepaths

--- multi_plink.sh
+++ multi_plink.sh
@@ -2,4 +2,4 @@
 
 for i in 1:22;
 do;
-plink --noweb  --bfile onekgenome_height_dmr --chr $i --r2 --ld-snp-list  ./ --ld-window 99999 --ld-window-kb 1000 --ld-window-r2 0.2 --out onekgenome_height_dmr_bpid_chr$i
+plink --noweb  --bfile onekgenome_height_dmr --chr $i --r2 --ld-snp-list  ../height_gwas_snp_1kgp_from_rsids --ld-window 99999 --ld-window-kb 1000 --ld-window-r2 0.2 --out ./onekgenome_height_dmr_bpid_chr$i;
